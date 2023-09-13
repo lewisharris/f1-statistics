bba9063298a44c41e6e8efa40c957549e3893479
Lewis <lewisdanielharris@gmail.com>
2023-09-13 23:43:10 +0100

    removed duplicate suspense import on layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata, Suspense } from "next";
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "../components/navigation/Navbar";
 import { Suspense } from "react";
